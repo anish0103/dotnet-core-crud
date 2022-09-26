737c2b2ff954240b53d6c9afdc716071987fb028
Anish Patel <anish.patel.1829405.ap@gmail.com>
2022-09-26 10:57:25 +0530

    Added UI, Removed Unwanted Files

--- src/CRUDApplication/Controllers/HomeController.cs
+++ src/CRUDApplication/Controllers/HomeController.cs
@@ -8,14 +8,22 @@ namespace CRUDApplication.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private readonly List<Customer> CustomerList;
+
         public HomeController(ILogger<HomeController> logger)
         {
+            CustomerList = new List<Customer>();
+            //{
+            //    new Customer() { Id = 1, Email = "xyz@gmail.com", Name = "XYZ1 Name" },
+            //    new Customer() { Id = 2, Email = "xyz2@gmail.com", Name = "XYZ2 Name" },
+            //    new Customer() { Id = 3, Email = "xyz3@gmail.com", Name = "XYZ3 Name" }
+            //};
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(CustomerList);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
